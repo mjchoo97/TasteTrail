8b8d3ee4ef0da92011354108d14b4057c368562d
mjchoo <mjchoo97@gmail.com>
2024-02-05 20:01:33 +0800

    return response

--- src/app/api/all/route.js
+++ src/app/api/all/route.js
@@ -4,10 +4,11 @@ import { connectToDB } from "../../../../lib/utils";
 
 export const GET = async (request) => {
   try {
-    connectToDB();
-    const recipes = await Recipe.find();
+    // connectToDB();
+    // const recipes = await Recipe.find();
     // console.log(recipes)
-    return NextResponse.json(recipes);
+    // return NextResponse.json(recipes);
+    return NextResponse.json(JSON.stringify({ status: "200" }));
   } catch (err) {
     console.log(err);
     throw new Error("Failed to fetch recipes");
